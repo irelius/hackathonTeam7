ca39f80ed04c88592d864f90ef607f83945e312a
irelius <kihong711@hotmail.com>
2023-11-17 20:07:25 -0800

    safety commit 2

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -6,48 +6,7 @@ import "./ProfilePage.css";
 
 function ProfilePage() {
   const sessionUser = useSelector((state) => state.session.user);
-<<<<<<< HEAD
-  const billingObj = useSelector((state) => state.address);
-  const reviewsObj = useSelector((state) => state.review);
-  const reviews = Object.values(reviewsObj);
-  const productObj = useSelector((state) => state.product);
-  const products = Object.values(productObj);
-
-  useEffect(() => {
-    if (sessionUser?.id === 1) {
-      dispatch(loadAllBillingsThunk());
-    } else {
-      dispatch(loadOneBillingThunk(sessionUser?.id));
-      dispatch(loadUserReviewsThunk(sessionUser?.id));
-      dispatch(loadAllProductsThunk());
-    }
-  }, [dispatch, sessionUser]);
-
-  const formatDate = (dateStr) => {
-    const date = new Date(dateStr);
-    const month = date.toLocaleString("default", { month: "short" }); // 'short' gives the abbreviated month name
-    const day = date.getDate();
-    const year = date.getFullYear();
-
-    return `${month} ${day}, ${year}`;
-  };
-
-  const renderStars = (rating) => {
-    const stars = [];
-    for (let i = 1; i <= 5; i++) {
-      if (i <= rating) {
-        // Render a full star
-        stars.push(<i key={i} className="bx bxs-star"></i>);
-      } else {
-        // Render an empty star
-        stars.push(<i key={i} className="bx bx-star"></i>);
-      }
-    }
-    return stars;
-  };
-=======
   const isAdmin = sessionUser?.id === 1;
->>>>>>> a29a2ddab7e552179f289a405c9ad9a2d383ec6a
 
   if (!sessionUser) {
     return <Redirect to="/login" />;
@@ -55,85 +14,12 @@ function ProfilePage() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className={`container profile ${reviews.length > 0 ? 'reviews-present' : ''}`}>
-        <div className="profile header">
-          <h1>
-            <i className="bx bxs-user-circle user-icon"></i>
-            {sessionUser.username}
-          </h1>
-        </div>
-        <div className="profile-content">
-          <div className="profile-left">
-            <div className="profile email">
-              <div className="info-header">
-                <h2>Personal information</h2>
-                <button className="info-edit-btn">
-                  Edit <i className="bx bx-pencil"></i>
-                </button>
-              </div>
-              <h3>Email</h3>
-              <li>{sessionUser.email}</li>
-            </div>
-            <div className="profile address">
-              <div className="info-header">
-                <h2>Address</h2>
-                <button className="info-edit-btn">
-                  Edit <i className="bx bx-pencil"></i>
-                </button>
-              </div>
-              <div className="address-row">
-                <div>
-                  <h3>Street</h3>
-                  <li>{billingObj.address}</li>
-                </div>
-                <div>
-                  <h3>State</h3>
-                  <li>{billingObj.billingState}</li>
-                </div>
-                <div>
-                  <h3>Postal Code</h3>
-                  <li>{billingObj.billingZipCode}</li>
-                </div>
-              </div>
-            </div>
-          </div>
-          {sessionUser?.id !== 1 ? (
-            <div className="profile-right">
-              <h2>Reviews</h2>
-              {reviews.map((review) => (
-                <div key={review.id}>
-                  <div className="review-info">
-                    <li className="info product">
-                      {
-                        products.find(
-                          (product) => product.id === review.productId
-                        )?.productName
-                      }
-                    </li>
-                    <li>{renderStars(review.rating)}</li>
-                  </div>
-                  <div className="review-info">
-                    <li>{review.review}</li>
-                  </div>
-                  <div className="review-info">
-                    <li> - {formatDate(review.createdAt)}</li>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
-=======
       <div>
         {isAdmin ? (
           <AdminDashboard />
         ) : (
           <CustomerDashboard />
         )}
->>>>>>> a29a2ddab7e552179f289a405c9ad9a2d383ec6a
       </div>
     </>
   );
