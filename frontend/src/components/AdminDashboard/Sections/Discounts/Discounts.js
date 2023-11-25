dec2b3f47e69f0e35a889c8d86d893466cdd610a
irelius <kihong711@hotmail.com>
2023-11-24 22:36:31 -0800

    applying css rules to admin dashboard main component

--- frontend/src/components/AdminDashboard/Sections/Discounts/Discounts.js
+++ frontend/src/components/AdminDashboard/Sections/Discounts/Discounts.js
@@ -1,6 +1,6 @@
 import "./Discounts.css"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import * as discountActions from "../../../../store/discount"
 
@@ -11,6 +11,13 @@ function DiscountsSection() {
     }, [dispatch])
 
     const discounts = Object.values(useSelector(state => state.discount))
+
+    const [expandRow, setExpandRow] = useState(null);
+
+    const handleRowClick = (index) => {
+        setExpandRow((prev) => (prev === index ? null : index));
+    };
+
 
     return (
         <div>
@@ -23,23 +30,24 @@ function DiscountsSection() {
                         <th>Expiration</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {discounts.map((el, i) => [
+                        <tr key={i} onClick={() => handleRowClick(i)}>
+                            <td>{el.discountName}</td>
+                            <td>{el.discountType.charAt(0).toUpperCase() + el.discountType.slice(1)}</td>
+                            <td>{el.discountValue}</td>
+                            <td>{el.expirationDate.slice(0, 10)}</td>
+                        </tr>,
+                        expandRow === i && (
+                            <tr key={`${i}-expanded`}>
+                                <td colSpan="4">
+                                    booba asdf asdf asdf asdf asdf asd f
+                                </td>
+                            </tr>
+                        ),
+                    ])}
+                </tbody>
             </table>
-            {discounts.map((el, i) => (
-                <tr key={i}>
-                    <td>
-                        {el.discountName}
-                    </td>
-                    <td>
-                        {el.discountType.charAt(0).toUpperCase() + el.discountType.slice(1)} {/* Discount type is all lower case, change so that it's capitalized */}
-                    </td>
-                    <td>
-                        {el.discountValue}
-                    </td>
-                    <td>
-                        {el.expirationDate.slice(0, 10)}
-                    </td>
-                </tr>
-            ))}
         </div>
     )
 }
